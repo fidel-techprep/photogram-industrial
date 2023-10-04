71506dc853700db8e1590a64264e7ef8bc22d649
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-10-04 13:42:39 +0000

    generate photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_172644) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_132959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
+    t.text "caption"
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_photos_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_172644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "users", column: "owner_id"
 end
